71d8349771e340598e51c1fd30660db2171f5a28
Angel Gabriel Negrete Valadez <42720094+anganevagg@users.noreply.github.com>
2021-10-11 15:54:22 -0500

    Falta fuerza neta

--- main.py
+++ main.py
@@ -1,44 +1,64 @@
 import math
 
-def sumaVector(v1, v2):
-    r = [0.0, 0.0]
-    for i in range(1,3):
-        r[i-1] = v2[i] - v1[i]
-    return r
+def sumaVector(v1, v2, r):
+	r = []
+	for i in range(R):
+		r.append(0.0)
+	for i in range(0, R):
+		r[i] = v2[i] - v1[i]
+	return r
 
 n = int(input("Ingresa el número de cargas a evaluar: "))
 R = int(input("Ingresa el número de dimensiones a evaluar: "))
 cargas=[]
-k=9e9
+posiciones=[]
+K=9e9
 for i in range(0, n):
-    cargas.append([])
-    cargas[i].append(float(input(f"Ingresa q para la carga {i+1}: ")))
-    cargas[i].append(float(input(f"Ingresa x para la carga {i+1}: ")))
-    cargas[i].append(float(input(f"Ingresa y para la carga {i+1}: ")))
+	cargas.append(float(input(f"Ingresa q para la carga {i+1}: ")))
+	posiciones.append([])
+	for j in range(0, R):
+		posiciones[i].append(float(input(f"Ingresa R{j+1} para la carga {i+1}: ")))
 
-print("cargas ",cargas)
-
+print("cargas ", cargas)
+print("posiciones", posiciones)
 r = []
 
 for i in range(n):
-    for j in range(n):
-        r.append(sumaVector(cargas[i], cargas[j]))
+	r.append([])
+	for j in range(n):
+		if j==i:
+			continue
+		r[i].append(sumaVector(posiciones[i], posiciones[j], R))
 
 print("vectores r", r)
 
 rn = []
 
-for i in range(1, n+1):
-    c=r[i]
-    rn.append(math.sqrt((c[0]**2+c[1]**2)))
-print("distancias", rn)
+for i in range(len(r)):
+	rn.append([])
+	for j in range(len(r[i])):
+		c=r[i][j]
+		sum = 0.0
+		for k in range(R):
+			sum+=(c[k]**2)
+		rn[i].append(math.sqrt(sum))
+print("distancias rn", rn)
+
+qr=[]
+for i in range(n):
+	qr.append([])
+	for j in range(n):
+		if(i==j):
+			continue
+		qr[i].append(cargas[i]*cargas[j])
 
 F=[]
 
-for i in range(len(r)):
-    F.append([0.0, 0.0])
-    for j in range(0,2):
-        qr=cargas[i][0]*cargas[j][0]
-        F[i][j]=rn[j]*(((k*qr)/rn[i]**2)*r[i][j])
+for i in range(n):
+	F.append([])
+	for j in range(len(r[i])):
+		F[i].append([])
+		for k in range(len(r[i][j])):
+			F[i][j].append(r[i][j][k]*(((K*qr[i][j])/(rn[i][j]**2))*(1/rn[i][j])))
 
-print(F)
+print("Fuerzas que ejerce cada carga F ", F)
